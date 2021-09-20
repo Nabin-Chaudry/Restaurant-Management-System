5fb0d3f4cbbe4059164ce506f01542d92786847c
Nabin Chaudry <nabin.clnc@gmail.com>
2021-09-20 20:03:28 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Restaurant Management System</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div id="particles-js"></div>
+			<nav>
+				<a href="index.php"><img src="images/logo.png" height="96" width="152"></a>
+				<ul>
+					<li><a href="index.php" onmouseover="onm.play()", onmousedown="onc.play()">Home</a></li>
+					<li><a href="about.php" onmouseover="onm.play()", onmousedown="onc.play()">About</a></li>
+					<li><a href="Contact.php" onmouseover="onm.play()", onmousedown="onc.play()">Contact
+					</a></li>
+					<li><a href="Sign.php" onmouseover="onm.play()", onmousedown="onc.play()">Register
+					</a></li>
+					<li><a href="reservation.php" onmouseover="onm.play()", onmousedown="onc.play()">Reservation
+					</a></li>
+					<li><a href="admin.php" onmouseover="onm.play()", onmousedown="onc.play()">Admin
+					</a></li>
+				</ul>
+			</nav>
+		</div>
+		<!-- particles.js container -->
+		<script type="text/javascript" src="js/particles.js"></script>
+		<script type="text/javascript" src="js/app.js"></script>
+	</header>
+		<script type="text/javascript" src="js/Beep.js"></script>
+		<script type="text/javascript" src="js/whistle.js"></script>
+</body> 
+</html>
